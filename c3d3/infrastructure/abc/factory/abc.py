ac125f2406917a90a09236a41a6e16312cf6aaa4
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-03-04 22:09:17 +0000

    Updated iFactory and iHandler

--- c3d3/infrastructure/abc/factory/abc.py
+++ c3d3/infrastructure/abc/factory/abc.py
@@ -1,12 +1,17 @@
-from abc import ABC
-from typing import Union, final
+from abc import ABC, abstractmethod
+from typing import final
 
-from c3d3.infrastructure.abc.handler.abc import iHandler
+from c3d3.core.decorators.classproperty.decorators import classproperty
+from c3d3.core.decorators.camel2snake.decorators import camel2snake
 
 
 class iFactory(ABC):
 
     __abc: dict = dict()
+
+    @abstractmethod
+    def __str__(self) -> str:
+        raise NotImplementedError
 
     @classmethod
     @final
@@ -21,3 +26,9 @@ class iFactory(ABC):
         if not obj:
             raise ValueError(f'Set value for {k} in {cls.__class__.__name__} factory')
         return obj
+
+    @final
+    @classproperty
+    @camel2snake
+    def key(self) -> str:
+        return self.__str__(self)
--- c3d3/infrastructure/abc/handler/abc.py
+++ c3d3/infrastructure/abc/handler/abc.py
@@ -1,13 +1,14 @@
 from abc import ABC, abstractmethod
-from typing import overload
+from typing import overload, final
 
 from c3d3.core.decorators.camel2snake.decorators import camel2snake
+from c3d3.core.decorators.classproperty.decorators import classproperty
 
 
 class iHandler(ABC):
 
     @abstractmethod
-    def __str__(cls) -> str:
+    def __str__(self) -> str:
         raise NotImplementedError
 
     @overload
@@ -17,3 +18,9 @@ class iHandler(ABC):
     @abstractmethod
     def do(self, *args, **kwargs):
         raise NotImplementedError
+
+    @final
+    @classproperty
+    @camel2snake
+    def key(self) -> str:
+        return self.__str__(self)
