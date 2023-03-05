86adda4f0bfda8994db78051ff13c7c4f17aaa0a
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-03-05 13:18:30 +0000

    Added GateIOSpotExchange

---
+++ c3d3/domain/c3/wrappers/gateio/spot/wrapper.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from c3d3.core.c3.interfaces.exchanges.interface import iCBE
+
+
+class GateIOSpotExchange(iCBE):
+    _ENDPOINT, _HEARTBEAT = "https://api.gateio.ws", ''
+
+    def tickers(
+            self,
+            currency_pair: Optional[str] = None,
+            timezone: Optional[str] = None
+    ) -> dict:
+        params = dict()
+        if currency_pair:
+            params.update({'currency_pair': currency_pair})
+        if timezone:
+            params.update({'timezone': timezone})
+        return self._r(
+            method=self._GET,
+            url='/api/v4/spot/tickers',
+            params=params
+        ).json()
