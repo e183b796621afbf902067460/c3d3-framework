cdb81e35f7b6e93e24e02a2dd7b10a8e6b08691a
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-03-09 21:05:35 +0000

    Added KyberSwapV3DexScreenerHandler

--- c3d3/infrastructure/d3/factories/dex_screener/factory.py
+++ c3d3/infrastructure/d3/factories/dex_screener/factory.py
@@ -4,6 +4,7 @@ from c3d3.infrastructure.d3.handlers.dex_screener.quickswap.v3.handler import Qu
 from c3d3.infrastructure.d3.handlers.dex_screener.quickswap.v2.handler import QuickSwapV2DexScreenerHandler
 from c3d3.infrastructure.d3.handlers.dex_screener.uniswap.v3.handler import UniSwapV3DexScreenerHandler
 from c3d3.infrastructure.d3.handlers.dex_screener.uniswap.v2.handler import UniSwapV2DexScreenerHandler
+from c3d3.infrastructure.d3.handlers.dex_screener.kyberswap.v3.handler import KyberSwapV3DexScreenerHandler
 
 
 class DexScreenerFactory(iFactory):
@@ -16,3 +17,4 @@ DexScreenerFactory.add_object(k=QuickSwapV2DexScreenerHandler.key, v=QuickSwapV2
 DexScreenerFactory.add_object(k=QuickSwapV3DexScreenerHandler.key, v=QuickSwapV3DexScreenerHandler)
 DexScreenerFactory.add_object(k=UniSwapV2DexScreenerHandler.key, v=UniSwapV2DexScreenerHandler)
 DexScreenerFactory.add_object(k=UniSwapV3DexScreenerHandler.key, v=UniSwapV3DexScreenerHandler)
+DexScreenerFactory.add_object(k=KyberSwapV3DexScreenerHandler.key, v=KyberSwapV3DexScreenerHandler)
---
+++ c3d3/infrastructure/d3/handlers/dex_screener/kyberswap/v3/handler.py
@@ -0,0 +1,111 @@
+from c3d3.domain.d3.wrappers.kyberswap.v3.pool.wrapper import KyberSwapV3PoolContract
+from c3d3.infrastructure.d3.interfaces.dex_screener.interface import iDexScreenerHandler
+from c3d3.domain.d3.adhoc.erc20.adhoc import ERC20TokenContract
+
+import datetime
+import requests
+
+from web3.middleware import geth_poa_middleware
+from web3._utils.events import get_event_data
+from web3.exceptions import TransactionNotFound
+from web3 import Web3
+from web3.exceptions import MismatchedABI
+
+
+class KyberSwapV3DexScreenerHandler(KyberSwapV3PoolContract, iDexScreenerHandler):
+    _FEE = None
+
+    def __str__(self):
+        return __class__.__name__
+
+    def __init__(
+            self,
+            api_key: str, chain: str,
+            start_time: datetime.datetime, end_time: datetime.datetime,
+            is_reverse: bool,
+            *args, **kwargs
+    ) -> None:
+        KyberSwapV3PoolContract.__init__(self, *args, **kwargs)
+        iDexScreenerHandler.__init__(self, api_key=api_key, chain=chain, start_time=start_time, end_time=end_time, is_reverse=is_reverse, *args, **kwargs)
+
+    def do(self):
+        r_start = requests.get(self.api_uri.format(timestamp=int(self.start.timestamp()))).json()['result']
+        r_end = requests.get(self.api_uri.format(timestamp=int(self.end.timestamp()))).json()['result']
+
+        start_block = int(r_start)
+        end_block = int(r_end)
+
+        w3 = Web3(self.provider)
+        w3.middleware_onion.inject(
+            geth_poa_middleware,
+            layer=0
+        )
+        self._FEE = self.swapFeeUnits() / 10 ** 5
+
+        t0_address, t1_address = self.token0(), self.token1()
+        t0 = ERC20TokenContract(address=t0_address, node=self.node)
+        t1 = ERC20TokenContract(address=t1_address, node=self.node)
+
+        t0_decimals, t1_decimals = t0.decimals(), t1.decimals()
+        t0_decimals, t1_decimals = t0_decimals if not self.is_reverse else t1_decimals, t1_decimals if not self.is_reverse else t0_decimals
+
+        t0_symbol, t1_symbol = t0.symbol(), t1.symbol()
+        pool_symbol = f'{t0_symbol}/{t1_symbol}' if not self.is_reverse else f'{t1_symbol}/{t0_symbol}'
+
+        event_swap, event_codec, event_abi = self.contract.events.Swap, self.contract.events.Swap.web3.codec, self.contract.events.Swap._get_event_abi()
+
+        overview = list()
+        while start_block < end_block:
+            events = w3.eth.get_logs(
+                {
+                    'fromBlock': start_block,
+                    'toBlock': start_block + self.chain.BLOCK_LIMIT,
+                    'address': self.contract.address
+                }
+            )
+            start_block += self.chain.BLOCK_LIMIT
+            for event in events:
+                try:
+                    event_data = get_event_data(
+                        abi_codec=event_codec,
+                        event_abi=event_abi,
+                        log_entry=event
+                    )
+                except MismatchedABI:
+                    continue
+                ts = w3.eth.getBlock(event_data['blockNumber']).timestamp
+                if ts > self.end.timestamp():
+                    break
+                sqrt_p, liquidity = event_data['args']['sqrtP'], event_data['args']['liquidity']
+
+                a0, a1 = event_data['args']['deltaQty0'], event_data['args']['deltaQty1']
+                a0, a1 = a0 if not self.is_reverse else a1, a1 if not self.is_reverse else a0
+
+                try:
+                    price = abs((a1 / 10 ** t1_decimals) / (a0 / 10 ** t0_decimals))
+                    receipt = w3.eth.get_transaction_receipt(event_data['transactionHash'].hex())
+                    recipient = receipt['to']
+                except (TransactionNotFound, ZeroDivisionError, KeyError):
+                    continue
+                overview.append(
+                    {
+                        'symbol': pool_symbol,
+                        'price': price,
+                        'sender': receipt['from'],
+                        'recipient': recipient,
+                        'amount0': a0,
+                        'amount1': a1,
+                        'decimals0': t0_decimals,
+                        'decimals1': t1_decimals,
+                        'sqrt_p': sqrt_p,
+                        'liquidity': liquidity,
+                        'fee': self._FEE,
+                        'gas_used': receipt['gasUsed'],
+                        'effective_gas_price': receipt['effectiveGasPrice'] / 10 ** 18,
+                        'gas_symbol': self.chain.NATIVE_TOKEN,
+                        'index_position_in_the_block': receipt['transactionIndex'],
+                        'tx_hash': event_data['transactionHash'].hex(),
+                        'time': datetime.datetime.utcfromtimestamp(ts)
+                    }
+                )
+        return overview
