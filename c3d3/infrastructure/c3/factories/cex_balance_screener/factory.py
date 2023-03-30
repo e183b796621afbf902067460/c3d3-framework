36f96a00721a7c1a271728c23b1ed2a10f4c3de6
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-03-30 12:18:39 +0000

    Added CexBalanceScreenerFactory

---
+++ c3d3/infrastructure/c3/factories/cex_balance_screener/factory.py
@@ -0,0 +1,14 @@
+from c3d3.infrastructure.abc.factory.abc import iFactory
+
+from c3d3.infrastructure.c3.handlers.cex_balance_screener.binance.spot.handler import BinanceSpotCexBalanceScreenerHandler
+from c3d3.infrastructure.c3.handlers.cex_balance_screener.binance.usdtm.handler import BinanceUsdtmCexBalanceScreenerHandler
+
+
+class CexBalanceScreenerFactory(iFactory):
+
+    def __str__(self):
+        return __class__.__name__
+
+
+CexBalanceScreenerFactory.add_object(k=BinanceSpotCexBalanceScreenerHandler.key, v=BinanceSpotCexBalanceScreenerHandler)
+CexBalanceScreenerFactory.add_object(k=BinanceUsdtmCexBalanceScreenerHandler.key, v=BinanceUsdtmCexBalanceScreenerHandler)
