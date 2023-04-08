3b591b363edb6c1c2f8f27883160aea8d4ce0080
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-04-08 11:29:53 +0000

    Updated D3AbstractFactory

--- c3d3/infrastructure/d3/abstract/factory.py
+++ c3d3/infrastructure/d3/abstract/factory.py
@@ -2,6 +2,7 @@ from c3d3.infrastructure._abc.factory.abc import iFactory
 
 from c3d3.infrastructure.d3.factories.dex_screener.factory import DexScreenerFactory
 from c3d3.infrastructure.d3.factories.dex_supply_screener.factory import DexSupplyScreenerFactory
+from c3d3.infrastructure.d3.factories.dex_borrow_screener.factory import DexBorrowScreenerFactory
 
 
 class D3AbstractFactory(iFactory):
@@ -12,4 +13,5 @@ class D3AbstractFactory(iFactory):
 
 D3AbstractFactory.add_object(k=DexScreenerFactory.key, v=DexScreenerFactory)
 D3AbstractFactory.add_object(k=DexSupplyScreenerFactory.key, v=DexSupplyScreenerFactory)
+D3AbstractFactory.add_object(k=DexBorrowScreenerFactory.key, v=DexBorrowScreenerFactory)
 
