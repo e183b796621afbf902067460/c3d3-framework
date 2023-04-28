45d57404f237814a981bfb8eed6ee0d1ba191bf7
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-04-28 08:50:14 +0000

    Updated setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     version="0.0.6",
     author="e183b796621afbf902067460",
     author_email="606d18446a06fe9738fd@gmail.com",
-    url="https://github.com/e183b796621afbf902067460/c3d3-research-framework",
+    url="https://github.com/e183b796621afbf902067460/c3d3-framework",
     packages=find_packages(),
     long_description=long_description,
     install_requires=required
