110905b3cde46096b23a58f63e99e32e789ccac3
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-05-19 15:10:14 +0000

    Updated setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as requirements:
 
 setup(
     name="c3d3",
-    version="0.0.9",
+    version="0.0.10",
     author="e183b796621afbf902067460",
     author_email="606d18446a06fe9738fd@gmail.com",
     url="https://github.com/e183b796621afbf902067460/c3d3-framework",
